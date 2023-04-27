2f684c54f6fce2fef2ba940e1de1861f15a054ea
Raymond Selzer <raymond@raymondselze.net>
2023-04-26 22:20:13 -0400

    get-videos 2.0 move parsed to parsed dir, add more console logging

--- get-videos.js
+++ get-videos.js
@@ -1,12 +1,16 @@
-const { existsSync, readdirSync, unlinkSync } = require("fs");
-const { exec } = require("child_process");
-
+const { existsSync, readdirSync, unlinkSync, renameSync } = require("fs");
+const { execSync } = require("child_process");
+const reader = require("./vosk-reader.js");
+const log = require("single-line-log").stdout;
+const { getVideoDurationInSeconds } = require("get-video-duration");
 const {
   getSchema,
   updateVideoInDB,
   videoExists,
   getVideoFields,
 } = require("./server/helpers");
+
+const { getClipsFromWords } = require("./helpers");
 
 const toParseDir = `./media/video/to-parse`;
 const parsedDir = `./media/video/parsed`;
@@ -26,17 +30,78 @@ const saveVideoData = (video) => {
   console.log("finished getting", video.id);
 };
 
-readdirSync(toParseDir).forEach((videoToParse) => {
+const readAudio = async (audioToParse, duration) => {
+  const results = [];
+
+  return new Promise((resolve) => {
+    reader(
+      audioToParse,
+      (words) => {
+        const result =
+          words?.alternatives[words.alternatives.length - 1]?.result;
+        if (result) {
+          const pos = result[result.length - 1].end;
+          const progress = `(${pos} / ${duration}) ${Math.round(
+            (pos / duration) * 100
+          )}%`;
+
+          results.push(words);
+
+          log(`${progress} "${result[result.length - 1].word}"`);
+        }
+      },
+      () => {
+        console.log();
+        resolve(results);
+      }
+    );
+  });
+};
+
+readdirSync(toParseDir).forEach(async (videoToParse) => {
   const audioToParse = `${rawAudioDir}/${videoToParse}.wav`;
+  const videoToParsePath = `${toParseDir}/${videoToParse}`;
+
+  console.log("=== Converting Video", videoToParse, " ===");
 
   if (existsSync(audioToParse)) {
     unlinkSync(audioToParse);
   }
 
-  exec(
-    `ffmpeg -i "${toParseDir}/${videoToParse}" -loglevel error -ac 1 -ar 16000 -acodec pcm_s16le "${audioToParse}"`,
-    (e, data, err) => {
-      console.log("got wav", { audioToParse, data });
-    }
+  const test = execSync(
+    `ffmpeg -i "${toParseDir}/${videoToParse}" -loglevel error -ac 1 -ar 16000 -acodec pcm_s16le "${audioToParse}"`
   );
+
+  console.log("Created ", audioToParse);
+
+  const duration = await getVideoDurationInSeconds(videoToParsePath);
+  const results = await readAudio(audioToParse, duration);
+
+  //get unique clips
+  const clips = getClipsFromWords(results).filter((clip, index, array) => {
+    return array.findIndex((clip2) => clip2.end === clip.end) === index;
+  });
+
+  console.log("found", clips.length, "clips");
+
+  clips.forEach((clip, i) => {
+    const start = Math.max(clip.end - 20, 0);
+    const end = Math.min(clip.end + 10, duration);
+
+    const clipPath = `${clipsAudioDir}/${videoToParse}_${i}.mp4`;
+
+    if (existsSync(clipPath)) {
+      unlinkSync(clipPath);
+    }
+
+    console.log("clipping", i + 1, "/", clips.length);
+
+    execSync(
+      `ffmpeg -i ${videoToParsePath}  -loglevel error -ss ${start} -to ${end} ${clipPath}`
+    );
+  });
+
+  console.log("moving", videoToParse, "to parsed");
+  renameSync(videoToParsePath, `${parsedDir}/${videoToParse}`);
+  console.log("DONE!");
 });
---
+++ helpers.js
@@ -0,0 +1,122 @@
+const secondsToHMS = (value) => {
+  const sec = parseInt(value, 10); // convert value to number if it's string
+  let hours = Math.floor(sec / 3600); // get hours
+  let minutes = Math.floor((sec - hours * 3600) / 60); // get minutes
+  let seconds = sec - hours * 3600 - minutes * 60; //  get seconds
+  // add 0 if value < 10; Example: 2 => 02
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + "h" + minutes + "m" + seconds + "s"; // Return is HH : MM : SS
+};
+
+const HMSToSeconds = (hms) => {
+  const hmsSplit = hms.split(":");
+
+  return (
+    parseInt(hmsSplit[0].replace(/^0/, "")) * 60 +
+    parseInt(hmsSplit[1].replace(/^0/, ""))
+  );
+};
+
+const hasClip = (word) => {
+  return (
+    word.trim() === "clip" ||
+    ["quip", "equip", "claire", "a twit", "cliff", "slip"].includes(word.trim())
+  );
+};
+
+const hasDelete = (word) => {
+  return (
+    word.trim() === "delete" ||
+    ["deleted", "d lead", "di lead", "dee lead"].includes(word.trim())
+  );
+};
+
+const alternativeHasClip = (alt) => {
+  if (alt.result === undefined) return false;
+
+  let ret = false;
+
+  alt.result.forEach((res) => {
+    if (hasClip(res.word)) {
+      ret = true;
+      return;
+    }
+  });
+
+  return ret;
+};
+
+const alternativeHasDelete = (alt) => {
+  if (alt.result === undefined) return false;
+
+  let ret = false;
+
+  alt.result.forEach((res) => {
+    if (hasDelete(res.word)) {
+      ret = true;
+      return;
+    }
+  });
+
+  return ret;
+};
+
+const getClipsFromWords = (words) => {
+  const alts = [];
+  const results = [];
+
+  words.forEach((word) => {
+    word.alternatives.forEach((alt) => {
+      if (alternativeHasClip(alt)) {
+        alts.push(alt);
+      }
+    });
+  });
+
+  //only return the first result in the alt matching the clip
+  alts.forEach((alt) => {
+    results.push(alt.result.find((result) => hasClip(result.word)));
+  });
+
+  return results;
+};
+
+const getDeletesFromWords = (words) => {
+  const results = [];
+  const alts = [];
+
+  words.forEach((word) => {
+    word.alternatives.forEach((alt) => {
+      if (alternativeHasDelete(alt)) {
+        alts.push(alt);
+      }
+    });
+  });
+
+  alts.forEach((alt) => {
+    results.push(alt.result.find((result) => hasDelete(result.word)));
+  });
+
+  return results;
+};
+
+module.exports = {
+  getDeletesFromWords,
+  getClipsFromWords,
+  hasClip,
+  hasDelete,
+  alternativeHasClip,
+  alternativeHasDelete,
+  HMSToSeconds,
+  secondsToHMS,
+  hasClip,
+  hasDelete,
+};
